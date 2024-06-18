5d4e78e2c64f753fe1480bf305e6828cb9fcd736
divyesh1008 <divyesh.patel@etatvasoft.com>
2024-06-18 09:46:27 +0530

    makeNewFile

---
+++ JobHunt.Domain/Helper/ConfigurationHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace JobHunt.Domain.Helper
+{
+    public static class ConfigurationHelper
+    {
+        private static IConfiguration _configuration = null!;
+
+        public static void Configure(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public static IConfigurationSection GetConfigurationSection(string section)
+        {
+            return _configuration.GetSection(section);
+        }
+    }
+}
---
+++ JobHunt.Domain/Helper/CustomAuthorize.cs
@@ -0,0 +1,87 @@
+﻿using JobHunt.Domain.DataModels.Response;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace JobHunt.Domain.Helper
+{
+    public class CustomAuthorize(string roleList = "") : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var unauthorizedResponse = new ResponseDTO()
+            {
+                IsSuccess = false,
+                StatusCode = System.Net.HttpStatusCode.Unauthorized,
+                ErrorMessages = [
+                    "Unauthenticated user"
+                ],
+                Message = "User is not authenticated."
+            };
+
+            var forbiddenResponse = new ResponseDTO()
+            {
+                IsSuccess = false,
+                StatusCode = System.Net.HttpStatusCode.Forbidden,
+                ErrorMessages = [
+                    "Unauthorized user"
+                ],
+                Message = "User is not authorize"
+            };
+
+            if(!context.HttpContext.Request.Headers.TryGetValue("Authorization", out var authorizationHeader))
+            {
+                context.Result = unauthorizedResponse;
+                return;
+            }
+
+            var bearerHeader = authorizationHeader.First();
+            if(bearerHeader != null)
+            {
+                context.Result = unauthorizedResponse;
+                return;
+            }
+
+            var tokenString = bearerHeader!.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries)[1];
+            if(tokenString == null || !Jwt.ValidateToken(tokenString, out JwtSecurityToken? jwtToken)) 
+            {
+                context.Result = unauthorizedResponse;
+                return;
+            }
+
+            if(jwtToken == null)
+            {
+                context.Result = unauthorizedResponse;
+                return;
+            }
+
+            var roles = roleList.Split(" ");
+
+            var claimRole = Jwt.GetClaimValue(ClaimTypes.Role, jwtToken);
+
+            if(claimRole != null || roles == null || roles.Length == 0 || string.IsNullOrEmpty(claimRole))
+            {
+                context.Result = forbiddenResponse;
+                return;
+            }
+
+            bool isValidRole = false;
+
+            foreach (var role in roles)
+            {
+                if (role == claimRole)
+                {
+                    isValidRole = true;
+                    return;
+                }
+            }
+
+            if (!isValidRole)
+            {
+                context.Result = forbiddenResponse;
+                return;
+            }
+
+        }
+    }
+}
